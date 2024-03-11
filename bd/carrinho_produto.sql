1b3541d8b21eac789cf2ab3a7c9a9e57322deb24
Bruno <brunorohling340@gmail.com>
2024-03-11 19:52:07 -0300

    mudei o banco

--- bd/carrinho_produto.sql
+++ bd/carrinho_produto.sql
@@ -1,21 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: carrinho
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
+-- Database: carrinho
 -- Table structure for table `produto`
 --
 
@@ -42,14 +26,4 @@ LOCK TABLES `produto` WRITE;
 INSERT INTO `produto` VALUES (1,'Calabresa','Salgados',6.50,1,'Salgados.jpg'),(2,'Hambúrguer','Salgados',6.50,1,'Salgados.jpg'),(3,'Frango com Bacon','Salgados',6.50,1,'Salgados.jpg'),(4,'Carne','Salgados',6.50,1,'Salgados.jpg'),(5,'Pirulito','Doces',1.00,1,'Doces.png'),(6,'Bala de Iogurte','Doces',0.20,1,'Doces.png'),(7,'Balas Branca','Doces',0.15,1,'Doces.png'),(8,'Trento','Doces',2.50,1,'Doces.png'),(9,'Canudo Frito','Doces',2.50,1,'Doces.png'),(10,'Fantasminha','Chips',2.50,1,'Chips.jpg'),(11,'Coca-Cola 220 mL','Bebidas',3.00,1,'Bebidas.jpg'),(12,'Fanta Laranja 220 mL','Bebidas',3.00,1,'Bebidas.jpg'),(13,'Sprite 220 mL','Bebidas',3.00,1,'Bebidas.jpg'),(14,'Suco Kapo 220 mL','Bebidas',3.00,1,'Bebidas.jpg');
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-03-11 19:44:17
